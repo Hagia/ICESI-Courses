9344cd9096a6b969805f7273d67d6b64f585043a
Hagia <mauricio.hernandez2@correo.icesi.edu.co>
2019-03-19 23:13:38 -0500

    First succesful rule

--- AI/ExpertSystemWorkshop/ExpertSystem.py
+++ AI/ExpertSystemWorkshop/ExpertSystem.py
@@ -1,18 +1,25 @@
 from pyknow import *
+from random import choice
 
 class Ground(Fact):
     pass
 
 class Drools(KnowledgeEngine):
 
-    @Rule(Ground(ph>=7.2))
+    @Rule(Ground(ph=L(7.2)))
     def ask_ph(self):
-        self.declare(Fact(ph=input("What's is the ground Ph?")))
-    @Rule(Ground(ph>6.8 & ph <7.2))
-    def ask_cd(self):
-        self.declare(Fact(cd=input("What's is the ground electricity condutivity?")))
+        print("==> ph: ALCALINO")
     
-    @Rule(Ground(ph <= 6.8 & ph > 6.2))
-    def ask_gt(self):
-        self.declare(Fact(ground_texture=input("What's is the ground texture?")))
+    # @Rule(AND(Ground(ph>6.8),Ground(ph<=7.2)))
+    # def ask_cd(self):
+    #     print("==> ph: LIGERAMENTE ALCALINO")
+    
+    # @Rule(AND(Ground(ph <= 6.8),Ground(ph > 6.2)))
+    # def ask_gt(self):
+    #     print("==> ph: NEUTRO")
+
+engine = Drools()
+engine.reset()
+engine.declare(Ground(ph=choice([7.2, 7.2, 7.2])))
+engine.run()
 
